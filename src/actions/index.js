74f333be850504c322ba84846940e99dae679b2c
Hassan Naeem <hnae6443@gmail.com>
2017-07-24 17:20:30 -0400

    Added redux action and reducer for delete functionality

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,18 @@
-import { 'ADD_REMINDER' } from '../constants';
+import { ADD_REMINDER } from '../constants';
+import { DELETE_REMINDER } from '../constants';
 
 export const addReminder = (text) => {
   const action = {
     type: ADD_REMINDER,
     text
   }
-  console.log('action', action);
+  return action;
+}
+
+export const deleteReminder = (id) => {
+  const action = {
+    type: DELETE_REMINDER,
+    id
+  }
   return action;
 }
